53e1c9e3a1e1bbc082373eff927de8c61dd892b9
Alex <alexan_oli@hotmail.com>
2023-11-11 19:20:55 -0600

    actualizacion de grid de cv

--- Curriculum Alexander Olivares/src/components/curriculum.jsx
+++ Curriculum Alexander Olivares/src/components/curriculum.jsx
@@ -3,37 +3,47 @@ import React from "react"
 const Curriculum = () => {
     return (
         <section className="curriculum">
-            <h3>Software skills</h3>
-            <ul>
+            <div className="software-skills">
+                <h3 className="cv-titles">Software skills</h3>
+            <ul className="card-container">
                 <li>HMTL5</li>
                 <li>CSS</li>
                 <li>Java</li>
                 <li>React</li>
                 <li>Git</li>
             </ul>
-            <h3>Idiomas</h3>
-            <ul>
-                <li>Ingles</li>
+            </div>
+            <div className="experience">
+            <h3 className="cv-titles">Experiencia</h3>
+            <ul className="card-container">
+                <li>Kavak</li>
+                <li>Oyster</li>
+                <li>Sabadell</li>
             </ul>
-            <h3>Soft Skills</h3>
-            <ul>
+            </div>
+            <div className="soft-skills">
+            <h3 className="cv-titles">Soft Skills</h3>
+            <ul className="card-container">
                 <li>Autodidacta</li>
                 <li>Responsable</li>
                 <li>Creativo</li>
                 <li>Analitico</li>
             </ul>
-            <h3>Experiencia</h3>
-            <ul>
-                <li>Kavak</li>
-                <li>Oyster</li>
-                <li>Sabadell</li>
-            </ul>
-            <h3>Eduacion</h3>
-            <ul>
+            </div>
+            <div className="education">
+            <h3 className="cv-titles">Eduacion</h3>
+            <ul className="card-container">
                 <li>Unitec - Ing Redes y software</li>
                 <li>Codeacademy - Front-end</li>
                 <li>Coderhouse - Product Manager</li>
             </ul>
+            </div>
+            <div className="languages">
+            <h3 className="cv-titles">Idiomas</h3>
+            <ul className="card-container">
+                <li>Ingles</li>
+            </ul>
+            </div>
         </section>
     )
 }
